acda607d2825f04087e72ce1a1723e18b8c16252
Munazer Montasir <50144800+AKASH-2019@users.noreply.github.com>
2025-09-04 13:04:46 +0600

    config yml start sh

--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
+# #!/bin/bash
+# rasa run --enable-api --cors "*" --host 0.0.0.0 &
+# rasa run actions --cors "*" --host 0.0.0.0 &
+# wait -n
+
+
 #!/bin/bash
-rasa run --enable-api --cors "*" --host 0.0.0.0 &
-rasa run actions --cors "*" --host 0.0.0.0 &
+set -e
+
+# Start Rasa server
+rasa run --enable-api --cors "*" --host 0.0.0.0 --port 5005 &
+
+# Start Action server
+rasa run actions --cors "*" --host 0.0.0.0 --port 5055 &
+
+# Wait for any process to exit
 wait -n
